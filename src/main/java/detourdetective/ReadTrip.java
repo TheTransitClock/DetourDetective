d99e1c0e495288cb861d9cf5c6574072f04910b1
Andrew <Andrewcunningham9911@hotmail.com>
2024-06-19 15:30:47 +0100

    Get trips between start date and end date. Get vehicle position with a start date almost working.

--- src/main/java/detourdetective/ReadTrip.java
+++ src/main/java/detourdetective/ReadTrip.java
@@ -2,11 +2,13 @@ package detourdetective;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ReadTrip {
@@ -34,5 +36,61 @@ public class ReadTrip {
             return null;
         }
     }
+    public static List<VehiclePosition> getTripVehiclePositions() {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<VehiclePosition> cr = cb.createQuery(VehiclePosition.class);
+            Root<VehiclePosition> root = cr.from(VehiclePosition.class);
+            Predicate tripIdPredicate = cb.like(root.get("trip_id"), "JG_A4-Weekday-SDon-084600_B16_414");
+            Predicate startDatePredicate = cb.like(root.get("trip_start_date"), "2024-03-26");
+
+            Predicate combinedPredicate = cb.and(tripIdPredicate, startDatePredicate);
+
+            cr.select(root).where(combinedPredicate);
+
+            Query<VehiclePosition> query = session.createQuery(cr);
+            List<VehiclePosition> results = query.getResultList();
+            // commit transaction
+            transaction.commit();
+            return results;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static List<VehiclePosition> getTripsBetweenStartDateAndEndDate(LocalDate startDate, LocalDate endDate) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<VehiclePosition> cr = cb.createQuery(VehiclePosition.class);
+            Root<VehiclePosition> root = cr.from(VehiclePosition.class);
+
+            cr.select(root).where(cb.between(root.get("trip_start_date"),startDate,endDate));
+
+            Query<VehiclePosition> query = session.createQuery(cr);
+            List<VehiclePosition> results = query.getResultList();
+            // commit transaction
+            transaction.commit();
+            return results;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 
 }
--- src/main/java/detourdetective/VehicleApp.java
+++ src/main/java/detourdetective/VehicleApp.java
@@ -1,11 +1,14 @@
 package detourdetective;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class VehicleApp {
     public static void main(String[] args) {
-        List<VehiclePosition> bus776Trip1 = ReadTrip.readtrip();
-        for (VehiclePosition position : bus776Trip1) {
+        LocalDate startdate = LocalDate.of(2024,03,27);
+        LocalDate endDate = LocalDate.of(2024,03,29);
+        List<VehiclePosition> between27And29March = ReadTrip.getTripsBetweenStartDateAndEndDate(startdate,endDate);
+        for (VehiclePosition position : between27And29March) {
             System.out.println(position);
         }
     }
