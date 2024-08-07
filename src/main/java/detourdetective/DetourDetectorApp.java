7dd08ff73f6f800597f5fb32985fb991df0f9507
Andrew <Andrewcunningham9911@hotmail.com>
2024-08-07 12:29:56 +0100

    Updated so no user input is needed. All done on commandLine.

--- src/main/java/detourdetective/DetourDetectorApp.java
+++ src/main/java/detourdetective/DetourDetectorApp.java
@@ -16,13 +16,13 @@ public class DetourDetectorApp {
 
 		CommandLineParser parser = new DefaultParser();
 
-		Scanner keyboard = new Scanner(System.in);
-
 		// create the Options
 		Options options = new Options();
-		options.addOption("R", "route", false, "This is the route we want to check..");
-		options.addOption("D", "date", false, "The date we want to check this route on..");
-		options.addOption("T", "detour", true, "Arguments: tripId vehicleId fileName");
+		options.addOption("R", "route", true, "This is the route we want to check..");
+		options.addOption("D", "date", true, "The date we want to check this route on..");
+		options.addOption("T", "tripId", true, "The trip id we will be checking..");
+		options.addOption("V", "vehicleId", true, "The vehicle id we will be checking on the trip id provided.");
+		options.addOption("F", "Filename", true, "The filename of where the output will be stored.");
 
 			try {
 				CommandLine cmd = parser.parse(options, args);
@@ -40,39 +40,37 @@ public class DetourDetectorApp {
 				}
 
 				if (cmd.hasOption("T")) {
-					String tripId;
-					String vehicleId;
-					String fileName;
+					System.out.println("Trip id : " + cmd.getOptionValue("T"));
+				} else {
+					System.out.println("Detour option not provided.");
+				}
 
-					System.out.println("Enter trip id:");
-					tripId = keyboard.next();
-					System.out.println("Enter vehicle id:");
-					vehicleId = keyboard.next();
-					System.out.println("Enter filename:");
-					fileName = keyboard.next();
-
-
-					System.out.println("Trip ID: " + tripId);
-					System.out.println("Vehicle ID: " + vehicleId);
-					System.out.println("File: " + fileName);
-
+				if (cmd.hasOption("V")){
+					System.out.println("Vehicle: " + cmd.getOptionValue("V"));
+				} else {
+					System.out.println("Vehicle not provided.");
+				}
+				if (cmd.hasOption("F")){
+					System.out.println("Filename: " + cmd.getOptionValue("F"));
+				} else {
+					System.out.println("Filename not provided.");
+				}
+				if (cmd.hasOption("T") && cmd.hasOption("V") && cmd.hasOption("F")) {
 					try {
 						// Initialize DetourDetector and perform detour detection
 						DetourDetector detourDetector = new DetourDetectorDefaultImpl();
-						List<List<VehiclePosition>> detours = detourDetector.detectDetours(tripId, vehicleId);
+						List<List<VehiclePosition>> detours = detourDetector.detectDetours(cmd.getOptionValue("T"), cmd.getOptionValue("V"));
 
 						// Export the detected detours to an Excel file
 						if (detours != null && !detours.isEmpty()) {
-							ExportToExcel.exportDetoursToExcel(detours, fileName);
-							System.out.println("Detours exported to " + fileName);
+							ExportToExcel.exportDetoursToExcel(detours, cmd.getOptionValue("F"));
+							System.out.println("Detours exported to " + cmd.getOptionValue("F"));
 						} else {
-							System.out.println("No detour detected for Vehicle " + vehicleId);
+							System.out.println("No detour detected for Vehicle " + cmd.getOptionValue("V"));
 						}
 					} catch (IOException e) {
 						System.out.println("IOException occurred: " + e.getMessage());
 					}
-				} else {
-					System.out.println("Detour option not provided.");
 				}
 			} catch (ParseException e) {
 				System.out.println("Unexpected exception: " + e.getMessage());
